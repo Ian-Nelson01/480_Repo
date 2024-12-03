f25eb4225af7b1cbf0f7e2332870f636dc8765b3
Connor Dunning <connorjdunning@gmail.com>
2024-12-03 07:57:17 -0800

    modified:   index.html 	modified:   modules/home_control.js 	modified:   modules/local-store.js

--- modules/local-store.js
+++ modules/local-store.js
@@ -1,26 +1,34 @@
 
-function permissions_test() {
-  localStorage.setItem("testKey", "testValue")
-  if (localStorage.getItem("testKey") == "testValue") {
-    console.log("Local Store working")
-    console.log(localStorage)
+export function permissions_test() {
+  try {
+    localStorage.setItem("testKey", "testValue")
+    if (localStorage.getItem("testKey") == "testValue") {
+      console.log("Local Store working")
+      console.log(localStorage)
+    }
+
+  } catch (error) {
+    console.log(error)
+    alert("Cookie Storage Error:\n" + error.toString())
   }
 
-} catch (error) {
-  console.log(error)
-  alert("Cookie Storage Error:\n" + error.toString())
 }
 
+// sym is JSON
+export function store_sym(sym) {
+  try {
+    localStorage.setItem("symbols", JSON.stringify(sym))
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-function store_sym(sym) {
-
+export function retrieve_sym() {
+  try {
+   return JSON.parse(localStorage.getItem("symbols")) 
+  } catch (e) {
+   console.error(e)
+  }
 }
 
-function retrieve_sym() {
 
-}
-
-
-
-export {permissions_test, store_sym, retrieve_sym}
